56f5095c3f3ef113eb83578f33bd811bab8cdd6d
Aron Jones <defrex0@gmail.com>
2011-05-31 14:06:32 -0400

    refined test output

--- test/phantomShim.js
+++ test/phantomShim.js
@@ -5,20 +5,26 @@ if (!phantom.state){
 }
 
 $(function(){
-  QUnit.begin = function(obj){
-    console.log('Stating Tests');
-  }
   QUnit.moduleStart = function(obj){
+    console.log(obj.name);
+  }
+  QUnit.testStart = function(obj){
     console.log('  '+obj.name);
   }
-  QUnit.testStart = function(obj){}
+  QUnit.log = function(obj){
+    var op = '    '
+    op += obj.result ? 'passed: ' : 'failed: ';
+    op += obj.message;
+    if (!obj.result && obj.expected && obj.actual)
+      op += ' (expected '+obj.expected+' got '+obj.actual+')'
+    console.log(op);
+  }
   QUnit.testDone = function(obj){
-    console.log('    '+obj.name);
-    console.log('      '+obj.failed+' failed, '+obj.passed+' passed.');
+    console.log('  '+obj.failed+' failed, '+obj.passed+' passed.');
   }
   QUnit.moduleDone = function(obj){
-    console.log('    Done: '+obj.name);
-    console.log('    '+obj.failed+' failed, '+obj.passed+' passed.');
+    console.log('Done: '+obj.name);
+    console.log(obj.failed+' failed, '+obj.passed+' passed.');
   }
   QUnit.done = function(obj){
     console.log(obj.total+' test run in '+obj.runtime+' miliseconds.');
