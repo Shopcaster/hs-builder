5685fd12e883145e3eeb23eda6cc07034e0a270a
Aron Jones <defrex0@gmail.com>
2011-06-13 17:39:47 -0400

    unglify js support

--- builder/steps/js/main.js
+++ builder/steps/js/main.js
@@ -2,12 +2,17 @@
 var fs = require('fs'),
     _ = require('underscore')._,
     cli = require('cli'),
-    exec = require('child_process').exec;
+    exec = require('child_process').exec,
+    ujs = require('uglify-js');
+    // ujsParse = require("uglify-js").parser,
+    // ujsUglify = require("uglify-js").uglify;
 
 exports.name = 'JavaScript';
 
 exports.options = {
   test: ['t', 'Build js with tests', 'boolean', false],
+  minify: ['m', 'Minify JS using Uglify JS', 'boolean', false],
+  pretify: ['p', 'Pretify minified JS using Uglify JS', 'boolean', false],
 };
 
 exports.build = function(opt, clbk){
@@ -33,60 +38,146 @@ exports.build = function(opt, clbk){
     if (err) return clbk(err);
     resolveDependancies(srcDir, function(err, files){
       if (err) return clbk(err);
-      var done = _.after(files.length, function(){clbk(null, {body: op})});
-      _.each(files, function(file){
-        // Javascript doesn't support negative lookbehinds in regex,
-        // but it does support negaive lookaheads!
-        var elif = file.split('').reverse().join('');
-        if (/^sj\.(?!tset_)/.test(elif) || (opt.test && /_test\.js$/.test(file))){
-          op += '<script src="js'+file.replace(srcDir, '')+'"></script>';
-          copyFile(file, file.replace(srcDir, buildDir), done);
-        }else if (/\.tmpl$/.test(file)){
-          fs.readFile(file, 'utf8', function(err, tmpl){
-            if (err) return clbk(err);
-            var name = /\/([\w-]+)\.tmpl$/.exec(file)[1];
-            op += '<script id="'+name+'" type="text/html">'
-            op += tmpl;
-            op += '</script>'
-            done();
-          });
-        }else if (/\.haml$/.test(file)){
-          fs.readFile(file, 'utf8', function(err, tmpl){
-            if (err) return clbk(err);
-            var name = /\/([\w-]+)\.haml$/.exec(file)[1];
-            var cmd = 'haml '+file;
-            cli.debug('HAML command: '+cmd);
-            exec(cmd, function(err, stdout, stderr){
+
+      var tmplNames = [];
+
+      (function scriptWrap(clbk){
+        if (files.tmpl)
+          (function nextTmpl(){
+            var tmpl = files.tmpl.shift();
+
+            if (!tmpl){
+              delete files.tmpl;
+              return scriptWrap(clbk);
+            }
+
+            fs.readFile(tmpl, 'utf8', function(err, tmplContent){
               if (err) return clbk(err);
-              tmpl = stdout;
-              cli.debug('Rendered HAML: '+tmpl);
+              var name = /\/([\w-]+)\.tmpl$/.exec(tmpl)[1];
+              tmplNames.push(name);
               op += '<script id="'+name+'" type="text/html">'
-              op += tmpl;
+              op += tmplContent;
               op += '</script>'
-              done();
+              nextTmpl();
             });
-          });
-        }else{
-          done();
-        }
+          })();
+        else if (files.haml)
+          (function nextHaml(){
+            var file = files.haml.shift();
+
+            if (!file){
+              delete files.haml;
+              return scriptWrap(clbk);
+            }
+
+            fs.readFile(file, 'utf8', function(err, tmpl){
+              if (err) return clbk(err);
+              var name = /\/([\w-]+)\.haml$/.exec(file)[1];
+              tmplNames.push(name);
+              var cmd = 'haml '+file;
+              cli.debug('HAML command: '+cmd);
+              exec(cmd, function(err, stdout, stderr){
+                if (err) return clbk(err);
+                tmpl = stdout;
+                cli.debug('Rendered HAML: '+tmpl);
+                op += '<script id="'+name+'" type="text/html">'
+                op += tmpl;
+                op += '</script>'
+                nextHaml();
+              });
+            });
+          })();
+        else if (files.js && !opt.minify)
+          (function nextJS(){
+            var file = files.js.shift();
+
+            if (!file){
+              delete files.js;
+              return scriptWrap(clbk);
+            }
+
+            op += '<script src="js'+file.replace(srcDir, '')+'"></script>';
+            copyFile(file, file.replace(srcDir, buildDir), nextJS);
+          })();
+        else if (files.js){
+          var js = '';
+
+          (function nextJSMin(){
+            var file = files.js.shift();
+
+            if (!file){
+
+              var comp;
+              var ast = ujs.parser.parse(js),
+                  ast = ujs.uglify.ast_mangle(ast, {toplevel: true}),
+                  ast = ujs.uglify.ast_squeeze(ast);
+
+              if (!opt.pretify)
+                comp = ujs.uglify.gen_code(ast);
+              else{
+                comp = ujs.uglify.gen_code(ast, {
+                  beautify: true,
+                  indent_start: 0,
+                  indent_level: 2
+                });
+              }
+
+              op += '<script>\n'+comp+'\n</script>';
+
+              delete files.js;
+              return scriptWrap(clbk);
+            }
+
+            fs.readFile(file, 'utf8', function(err, script){
+              js += '\n\n'+script;
+              nextJSMin();
+            });
+
+          })();
+        }else if (files.test && opt.test)
+          (function nextTest(){
+            var file = files.test.shift();
+
+            if (!file){
+              delete files.test;
+              return scriptWrap(clbk);
+            }
+
+            op += '<script src="js'+file.replace(srcDir, '')+'"></script>';
+            copyFile(file, file.replace(srcDir, buildDir), nextTest);
+          })();
+        else
+          clbk();
+      })(function(){
+        clbk(null, {body: op});
       });
+
     });
   });
 };
 
+/**
+* calls clbk(error, files) with any javascript file dependancies resolved.
+* files: {extension: [filename, ...]}
+**/
 function resolveDependancies(dir, clbk){
   createFileList(dir, function(err, fileList){
     if (err) return clbk(err);
-    makeDepList(dir, fileList, function(err, depList){
+    fileList = sortByExt(fileList);
+    makeDepList(dir, fileList.js, function(err, depList){
       if (err) return clbk(err);
       resolveDepList(depList, function(err, resolvedFiles){
         if (err) return clbk(err);
-        clbk(null, resolvedFiles);
+        fileList.js = resolvedFiles;
+        clbk(null, fileList);
       });
     });
   });
 }
 
+/**
+* calls clbk(error, files) with a list of any files in the passed directory
+**/
 function createFileList(dir, clbk){
   var fileList = [];
   fs.readdir(dir, function(err, filenames){
@@ -114,6 +205,35 @@ function createFileList(dir, clbk){
   });
 }
 
+/**
+* takes a list of filenames, and returns an object
+* {fileExtension: [filename, ...]}. The only exception is any _test.js
+* files, which will be in resp.test
+**/
+function sortByExt(files){
+  var resp = {
+    test: [],
+  };
+
+  files.forEach(function(file){
+    if (/_test\.js$/.test(file))
+      return resp.test.push(file);
+
+    var parsed = /\.(\w+)$/.exec(file);
+
+    if (parsed){
+      resp[parsed[1]] = resp[parsed[1]] || [];
+      resp[parsed[1]].push(file);
+    }
+  });
+  return resp;
+}
+
+/**
+* takes a root directory, a list of files, and a callback
+* calls clbk(error, dependancyList) where dependancyList
+* is in the form: {file: [dep1, dep2, ...]}
+**/
 function makeDepList(root, fileList, clbk){
   var depList = {},
       reg = /(?:\/\/|<!--)depends:([\w\-\.\s\n\/,]+?[^,])[\n\r]+/;
@@ -143,6 +263,11 @@ function makeDepList(root, fileList, clbk){
   })();
 }
 
+/**
+* takes a dependancy object in the form: {file: [dep1, dep2, ...]}
+* and calls clbk(error, files) where files is an array of files
+* ordered so dependancies are resolved
+**/
 function resolveDepList(depList, clbk){
   var resolvedList = [],
       revs = 0,
